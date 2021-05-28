78ec756a56dd2f9fcdd16c82b99de4baefc5362e
Ivan Efimov <gendalf44@yandex.ru>
2021-05-28 02:12:27 -0500

    Added indexer/check.js to run verifications without creating index.json

--- indexer/indexer.js
+++ indexer/indexer.js
@@ -9,23 +9,36 @@ const crypto = require('crypto');
 const errors = [];
 process.exitCode = 100;
 
+let writeIndexFile = true;
+
+if (process.argv.length === 3) {
+    if (process.argv[2] === "nosave") {
+        writeIndexFile = false;
+    }
+}
+
 let presetFilesArray = [];
 let presetsFolder = new PresetsFolder(settings.presetsDir, settings, presetFilesArray, errors);
 
 //console.log(getUniqueValues(presetFilesArray, "firmwareVersion"));
 
-if (0 === errors.length)
-{
-    const indexContent = new IndexContent(presetFilesArray, settings);
-    const jsonIndexContent = JSON.stringify(indexContent, null, 2);
-    fs.writeFileSync("index.json", jsonIndexContent);
-    console.log("OK: index.json created");
+if (0 === errors.length) {
+    console.log("OK");
 
-    const sum = crypto.createHash('sha256');
-    sum.update(jsonIndexContent);
-    const indexHash = sum.digest('hex');
-    fs.writeFileSync("index_hash.txt", indexHash);
-    console.log("OK: index_hash.txt created");
+    if (writeIndexFile) {
+        const indexContent = new IndexContent(presetFilesArray, settings);
+        const jsonIndexContent = JSON.stringify(indexContent, null, 2);
+        fs.writeFileSync("index.json", jsonIndexContent);
+        console.log("index.json created");
 
-    process.exitCode = 0;
+        const sum = crypto.createHash('sha256');
+        sum.update(jsonIndexContent);
+        const indexHash = sum.digest('hex');
+        fs.writeFileSync("index_hash.txt", indexHash);
+        console.log("index_hash.txt created");
+
+        process.exitCode = 0;
+    }
+} else {
+    console.log("Failed with errors");
 }
