fbdb3e81870f71cad3b1a09f7a333871eb7d86c3
blckmn <blackman@xtra.com.au>
2021-10-11 08:13:23 +1100

    Updated to main (default branch)
    - now runs action on both a push and a pull request
    - updated descriptions for the jobs
    - checker is run only on pull request, and a deploy is run on a push to main

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,14 +18,19 @@ echo "Bucket:    ${AWS_BUCKET}"
 mkdir -p ${artifacts}
 
 node --version
-node indexer/indexer.js
-
-echo "Copying files to staging location: ${artifacts}"
-cp ./index.json $artifacts
-cp ./index_hash.txt $artifacts 
-cp -r ./presets/* $artifacts
+node indexer/check.js
 
 if [ "${1}" == "deploy" ]; then
+
+  echo "Running indexer"
+  node indexer/indexer.js
+
+  echo "Copying files to staging location: ${artifacts}"
+  cp ./index.json $artifacts
+  cp ./index_hash.txt $artifacts 
+  cp -r ./presets/* $artifacts
+
+  echo "Deploying to AWS S3: ${AWS_BUCKET}"
   aws configure set preview.cloudfront true
   aws s3 sync ${artifacts} s3://${AWS_BUCKET}/presets --delete --region "${AWS_REGION}" --cache-control max-age=345600
   aws cloudfront create-invalidation --distribution-id ${AWS_DISTRIBUTION_ID} --path "/presets/*"
