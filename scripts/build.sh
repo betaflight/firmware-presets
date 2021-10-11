59a6a1509b188b9cd3eb56f9639b59d468cdfe6c
J Blackman <blckmn@users.noreply.github.com>
2021-10-11 17:36:42 +1100

    Updated path (to match repo)

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,22 +5,21 @@ set -e
 basepath="${PWD}"
 artifacts="${basepath}/public"
 
-if [ -d ${artifacts} ]; then
-  rm -R ${artifacts}
-fi
-
 echo "Artifacts: ${artifacts}"
 echo "Branch:    ${GITHUB_REF}"
 echo "Build:     ${GITHUB_RUN_NUMBER}"
-echo "Region:    ${AWS_REGION}"
-echo "Bucket:    ${AWS_BUCKET}"
-
-mkdir -p ${artifacts}
 
 node --version
 node indexer/check.js
 
 if [ "${1}" == "deploy" ]; then
+
+  if [ -d ${artifacts} ]; then
+    rm -R ${artifacts}
+  fi
+
+  mkdir -p ${artifacts}
+  mkdir -p ${artifacts}/presets
 
   echo "Running indexer"
   node indexer/indexer.js
@@ -28,10 +27,10 @@ if [ "${1}" == "deploy" ]; then
   echo "Copying files to staging location: ${artifacts}"
   cp ./index.json $artifacts
   cp ./index_hash.txt $artifacts 
-  cp -r ./presets/* $artifacts
+  cp -r ./presets/* $artifacts/presets
 
-  echo "Deploying to AWS S3: ${AWS_BUCKET}"
+  echo "Deploying to AWS S3: ${AWS_REGION}/${AWS_BUCKET}"
   aws configure set preview.cloudfront true
-  aws s3 sync ${artifacts} s3://${AWS_BUCKET}/presets --delete --region "${AWS_REGION}" --cache-control max-age=345600
-  aws cloudfront create-invalidation --distribution-id ${AWS_DISTRIBUTION_ID} --path "/presets/*"
+  aws s3 sync ${artifacts} s3://${AWS_BUCKET}/firmware-presets --delete --region "${AWS_REGION}" --cache-control max-age=345600
+  aws cloudfront create-invalidation --distribution-id ${AWS_DISTRIBUTION_ID} --path "/firmware-presets/*"
 fi
