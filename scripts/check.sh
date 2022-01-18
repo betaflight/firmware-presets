9f804b92116773b3a7091553d461c538547e4ce9
Philipp Molitor <philipp@molitor-consulting.com>
2022-01-18 23:56:42 +0100

    remove unused check flag

--- scripts/check.sh
+++ scripts/check.sh
@@ -4,4 +4,4 @@ set -e
 SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 export PRESETS_DIR="${SCRIPT_DIR}/../presets"
 
-node "${SCRIPT_DIR}/indexer/index.js" check
+node "${SCRIPT_DIR}/indexer/index.js"
