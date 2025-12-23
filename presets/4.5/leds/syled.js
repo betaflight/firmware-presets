795222459db225e4b857f623f2046c01f2c53757
caibin <caibinguitar@gmail.com>
2025-12-23 21:31:33 +0800

    add syled

--- presets/4.5/leds/syled.js
+++ presets/4.5/leds/syled.js
@@ -2,7 +2,7 @@
   "board": "MAMBAF405US_I2C",          // 飞控板型（必须与目录名一致，不可随意修改）
   "name": "MyCustom F405 Racing", // 你的预设名称（地面站中显示的名称）
   "description": "我的专属F405竞速配置，优化了油门响应", // 预设描述（可选）
-  "firmwareVersion": "4.4.0", // 适配的Betaflight固件版本
+  "firmwareVersion": "4.5.1", // 适配的Betaflight固件版本
   "config": {               // 核心配置参数（可根据需求修改）
     "mc_profile": 0,
     "pid_controller": "PID",
